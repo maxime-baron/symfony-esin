023e443e2fb023ded64a454c07ecf3b9f57d916c
Maxime <maxime.baron2002@gmail.com>
2022-12-11 18:29:11 +0100

    Devoir fais, je crois

--- app/src/Controller/CheckoutController.php
+++ app/src/Controller/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Checkout;
 use App\Entity\CheckoutProduct;
 use App\Form\CheckoutCreateType;
+use App\Repository\CheckoutProductRepository;
 use App\Repository\ProductRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,10 +16,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CheckoutController extends AbstractController
 {
     #[Route('/checkout', name: 'app_checkout')]
-    public function index(): Response
+    public function index(CheckoutProductRepository $CheckoutProductRepository): Response
     {
+        $checkouts = $CheckoutProductRepository->findAll();
         return $this->render('checkout/index.html.twig', [
             'controller_name' => 'CheckoutController',
+            'checkouts' => $checkouts,
         ]);
     }
 
@@ -65,7 +68,7 @@ class CheckoutController extends AbstractController
             return $this->redirectToRoute('app_checkout_read', ['id' => $checkout->getId()]);
         }
 
-        return $this->render('checkout/index.html.twig', [
+        return $this->render('checkout/create.html.twig', [
             'controller_name' => 'CheckoutController',
             'form' => $form->createView(),
             'product' => $product
@@ -78,7 +81,7 @@ class CheckoutController extends AbstractController
     public function read(Checkout $checkout): Response
     {
         // @todo: modifier le template pour afficher le checkout et un formulaire pour le modifier
-        return $this->render('checkout/index.html.twig', [
+        return $this->render('checkout/checkout.html.twig', [
             'controller_name' => 'CheckoutController',
             'checkout' => $checkout
         ]);
